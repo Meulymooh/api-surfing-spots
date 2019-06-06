1bdd262cb1d509d9b22dea0ce733ed76eebbf19c
Emilie Mesureur <mesureur.e@gmail.com>
2019-06-06 16:44:28 +0200

    created cards

--- javascript.js
+++ javascript.js
@@ -4,20 +4,27 @@ var surfingSpots = document.querySelector('.surfingSpots');
 fetch('http://api.spitcast.com/api/spot/all')
   .then(response => response.json())
   .then(data => {
-    listSpots(surfingSpots, data) // Prints result from `response.json()` in getRequest
+    listSpots(surfingSpots, data);
   })
   .catch(error => console.error(error))
 
 
 // List spots
-function listSpots (listDiv, spots) {
-  console.log(spots)
-  let ul = document.createElement('ul')
+function listSpots (surfingSpots, spots) {
+  // Creating bootstrap cards
   spots.forEach(spot => {
-    let li = document.createElement('li')
-    li.innerHTML = spot.spot_name
-    ul.append(li)
+  let card = document.createElement("div");
+      card.setAttribute("class", "card");
+  let body = document.createElement("div");
+      body.setAttribute("class", "card-body");
+  let title = document.createElement("div");
+      title.setAttribute("class", "card-title");
+  let info = document.createElement("div");
+      info.setAttribute("class", "card-text");
+    surfingSpots.append(card);
+    card.append(body);
+    body.append(title);
+    body.append(info);
+    title.innerHTML = "<h4>" + spot.spot_name + "</h4>";
   })
-  console.log(ul)
-  listDiv.append(ul)
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Link: 
+Link: https://meulymooh.github.io/api-surfing-spots/index.html
 
 # API: California's Surfing Spots
 	                                                                                                                                                                                                                                                                                                                                                                                                                               
